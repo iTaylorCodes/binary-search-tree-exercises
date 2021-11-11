261f5da67aa628a616e64f262080bafb6a074017
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-11-10 20:45:33 -0800

    Completed basic bst methods

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -21,13 +21,13 @@ class BinarySearchTree {
 
     let current = this.root;
     while (current !== newNode) {
-      if (newNode.val < current.val) {
+      if (val < current.val) {
         if (current.left === null) {
           current.left = newNode;
         }
         current = current.left;
       }
-      if (newNode.val > current.val) {
+      if (val > current.val) {
         if (current.right === null) {
           current.right = newNode;
         }
@@ -40,37 +40,123 @@ class BinarySearchTree {
   /** insertRecursively(val): insert a new node into the BST with value val.
    * Returns the tree. Uses recursion. */
 
-  insertRecursively(val) {}
+  insertRecursively(val, current = this.root) {
+    const newNode = new Node(val);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    if (val < current.val) {
+      if (current.left === null) {
+        current.left = newNode;
+      }
+      return this.insertRecursively(val, current.left);
+    }
+    if (val > current.val) {
+      if (current.right === null) {
+        current.right = newNode;
+      }
+      return this.insertRecursively(val, current.right);
+    }
+    return this;
+  }
 
   /** find(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses iteration. */
 
-  find(val) {}
+  find(val) {
+    if (!this.root) return undefined;
+
+    let current = this.root;
+    while (current.val !== val) {
+      if (val < current.val) {
+        if (!current.left) return undefined;
+        current = current.left;
+      }
+      if (val > current.val) {
+        if (!current.right) return undefined;
+        current = current.right;
+      }
+    }
+
+    return current;
+  }
 
   /** findRecursively(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses recursion. */
 
-  findRecursively(val) {}
+  findRecursively(val, current = this.root) {
+    if (!current) return undefined;
+
+    if (val < current.val) {
+      if (!current.left) return undefined;
+      return this.findRecursively(val, current.left);
+    } else if (val > current.val) {
+      if (!current.right) return undefined;
+      return this.findRecursively(val, current.right);
+    } else {
+      return current;
+    }
+  }
 
   /** dfsPreOrder(): Traverse the array using pre-order DFS.
    * Return an array of visited nodes. */
 
-  dfsPreOrder() {}
+  dfsPreOrder(visited = [], current = this.root) {
+    visited.push(current.val);
+
+    if (current.left) this.dfsPreOrder(visited, current.left);
+
+    if (current.right) this.dfsPreOrder(visited, current.right);
+
+    return visited;
+  }
 
   /** dfsInOrder(): Traverse the array using in-order DFS.
    * Return an array of visited nodes. */
 
-  dfsInOrder() {}
+  dfsInOrder(visited = [], current = this.root) {
+    if (current.left) this.dfsInOrder(visited, current.left);
+
+    visited.push(current.val);
+
+    if (current.right) this.dfsInOrder(visited, current.right);
+
+    return visited;
+  }
 
   /** dfsPostOrder(): Traverse the array using post-order DFS.
    * Return an array of visited nodes. */
 
-  dfsPostOrder() {}
+  dfsPostOrder(visited = [], current = this.root) {
+    if (current.left) this.dfsPostOrder(visited, current.left);
+
+    if (current.right) this.dfsPostOrder(visited, current.right);
+
+    visited.push(current.val);
+
+    return visited;
+  }
 
   /** bfs(): Traverse the array using BFS.
    * Return an array of visited nodes. */
 
-  bfs() {}
+  bfs() {
+    const visited = [];
+
+    const toVisitQueue = [this.root];
+
+    while (toVisitQueue.length) {
+      let current = toVisitQueue.shift();
+
+      if (current.left) toVisitQueue.push(current.left);
+      if (current.right) toVisitQueue.push(current.right);
+      visited.push(current.val);
+    }
+    return visited;
+  }
 
   /** Further Study!
    * remove(val): Removes a node in the BST with the value val.
